5c9c106152c9007928d940b23bdfd7df8ec52901
khushi <khushijaiswal343@gmail.com>
2026-06-23 03:13:44 +0530

    test: Fix contextLoads by mocking RedisTemplate to satisfy UrlCacheService dependency

--- src/test/java/com/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/urlshortener/UrlShortenerApplicationTests.java
@@ -4,9 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class UrlShortenerApplicationTests {
+
+    @MockBean
+    private RedisTemplate<String, String> redisTemplate;
 
     @Test
     void contextLoads() {
